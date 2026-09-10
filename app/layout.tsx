4fb8c797d19b86a2734af6118003ceac96f18b02
Nexvyn <amangupta.ans@gmail.com>
2026-09-10 16:51:04 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Caveat, Geist, Geist_Mono, Gemunu_Libre, Inter, Inter_Tight } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
@@ -154,6 +155,7 @@ export default function RootLayout({
             >
               {children}
               <Toaster position="top-center" />
+              <Analytics />
             </ThemeProvider>
           </ShapeProvider>
         </MotionProvider>
