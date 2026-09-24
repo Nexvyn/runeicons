bdcb24b8e99bc706aa68e19de68987771b5c4b22
Nexvyn <amangupta.ans@gmail.com>
2026-09-25 02:39:41 +0530

    build runeicons-svelte with svelte 5 runes and generated icon data

---
+++ packages/runeicons-svelte/src/RuneIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { getIconById, type IconType } from "./icons";
+
+  interface Props {
+    name: string;
+    type?: IconType;
+    size?: number;
+  }
+
+  let { name, type = "normal", size = 24 }: Props = $props();
+
+  const variant = $derived(getIconById(name)?.variants[type]);
+</script>
+
+{#if variant}
+  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox={variant.viewBox}>{@html variant.markup}</svg>
+{/if}
